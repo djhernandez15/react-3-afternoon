379b6ab0056c00e8dcbdc6446d3d92ac30fec98f
David Hernandez <djhernandez@Davids-MacBook-Air.local>
2019-03-29 08:16:44 -0500

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post';
 
 import './App.css';
 
@@ -19,15 +21,27 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios
+      .get('https://practiceapi.devmountain.com/api/posts')
+      .then(response => {
+        this.setState({posts: response.data})
+      })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+    .put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text })
+    .then(response => {
+      this.setState({posts: response.data})
+    });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+    .delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`)
+    .then(response => {
+      this.setState({posts: response.data})
+    })
   }
 
   createPost() {
@@ -44,7 +58,16 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+          {posts.map(post => 
+            (<Post 
+              key={post.id} 
+              text={post.text} 
+              date={post.date} 
+              updatePostFn={this.updatePost()}
+              id={post.id}
+              deletePostFn={this.deletePost()}
+              />)
+          )}
         </section>
       </div>
     );
